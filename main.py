b3eb471f2b06c0d35ed5800d8f210c8dc4e4cd91
RianValcanaia <riancarlosvalcanaia99@gmail.com>
2025-12-11 22:50:21 -0300

    refact: para verificar funcionamento do parser

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 from src.path_manager import PathManager
 from src.config_loader import ConfigLoader
 from src.network_controller import NetworkController
+from src.parser import ConfigParser
 from src.colors import Colors as co
+
+def valida_configuracoes():
+    co.infoln("Iniciando validação do arquivo network.yaml")
+
+    paths = PathManager()
+
+    try:
+        loader = ConfigLoader(paths.network_yaml, paths.versions_yaml)
+        config = loader.load()
+    except Exception as e:
+        co.errorln(f"Erro ao ler arquivos YAML: {e}")
+        return
+    
+    parser = ConfigParser(config)
+
+    parser.valida()
 
 def create_network():
     # 1. Configurar Caminhos
@@ -46,21 +63,22 @@ def clean_files():
         return
 
 def main():
+    valida_configuracoes()
 
-    print("Digite a opcao desejada:")
-    print("1 - Criar network")
-    print("2 - Limpar arquivos gerados e network")
-    print("0 - sair")
-    opcao = input()
+    # print("Digite a opcao desejada:")
+    # print("1 - Criar network")
+    # print("2 - Limpar arquivos gerados e network")
+    # print("0 - sair")
+    # opcao = input()
 
-    if opcao == '1':
-        create_network()
-    elif opcao == '2':
-        clean_files()
-    elif opcao == '0':
-        print("Saindo...")
-    else:
-        print("Opcao invalida.")
+    # if opcao == '1':
+    #     create_network()
+    # elif opcao == '2':
+    #     clean_files()
+    # elif opcao == '0':
+    #     print("Saindo...")
+    # else:
+    #     print("Opcao invalida.")
 
 
 if __name__ == "__main__":
