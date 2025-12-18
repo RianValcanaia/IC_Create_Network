6c8a262efba79f495e928824b6b594fb00af1cc2
RianValcanaia <riancarlosvalcanaia99@gmail.com>
2025-12-18 20:34:06 -0300

    refact: adding exceptions in parser

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ def _valida_configuracoes(config):
     parser = ConfigParser(config)
     parser.valida()
 
+    if parser.erros:
+        raise RuntimeError("Erros de validação encontrados.")
+
+
 def _cria_compose_ca(config, paths):
     compose_generator = ComposeGenerator(config, paths)
     compose_generator.generate_ca_compose()
@@ -48,27 +52,27 @@ def _cria_artefatos(controller, config, paths):
     configtx_gen = ConfigTxGenerator(config, paths)
     configtx_gen.generate()
 
-    # try:
-    #     controller.run_script("create_artifacts.sh")
-    # except Exception as e:
-    #     co.errorln(f"\n Erro ao rodar 'create_artifacts.sh': {e}")
-    #     return
+    try:
+        controller.run_script("create_artifacts.sh")
+    except Exception as e:
+        co.errorln(f"\n Erro ao rodar 'create_artifacts.sh': {e}")
+        return
 
 def network_up(controller, config, paths):
-    # # ------------- Preparando o ambiente --------------
-    # co.infoln("Iniciando a rede")
-    # co.infoln("Verificando pré-requisitos do sistema")
-    # _verifica_prerequisitos(controller)
-    # co.infoln("Validando configurações do arquivo network.yaml")
-    # _valida_configuracoes(config)
-    # co.infoln("Gerando arquivos docker-compose para ca")
-    # _cria_compose_ca(config, paths)
+    # ------------- Preparando o ambiente --------------
+    co.infoln("Iniciando a rede")
+    co.infoln("Verificando pré-requisitos do sistema")
+    _verifica_prerequisitos(controller)
+    co.infoln("Validando configurações do arquivo network.yaml")
+    _valida_configuracoes(config)
+    co.infoln("Gerando arquivos docker-compose para ca")
+    _cria_compose_ca(config, paths)
 
-    # # ------------- Iniciando a network --------------
-    # co.infoln("Iniciando os servidores CA")
-    # _start_CA(controller)
-    # co.infoln("Registrando e matriculando identidades")
-    # _register_enroll(controller, config, paths)
+   # ------------- Iniciando a network --------------
+    co.infoln("Iniciando os servidores CA")
+    _start_CA(controller)
+    co.infoln("Registrando e matriculando identidades")
+    _register_enroll(controller, config, paths)
     co.infoln("Gerando artefatos da rede (configtx.yaml, blocos, canais, etc)")
     _cria_artefatos(controller, config, paths)
 
@@ -83,19 +87,23 @@ def clean_files(controller, op = 1):
         return
 
 def main():
-    # 1. Configurar Caminhos
-    paths = PathManager()
+    try:
+        # configura caminhos
+        paths = PathManager()
 
-    # 2. Carregar Configurações
-    loader = ConfigLoader(paths.network_yaml, paths.versions_yaml)
-    config = loader.load()
+        # carregar configuracoes
+        loader = ConfigLoader(paths.network_yaml, paths.versions_yaml)
+        config = loader.load()
 
-    # 3. Inicializar Controlador
-    controller = NetworkController(config, paths)
-    paths.ensure_network_dirs()
+        # inicializa controller da rede
+        controller = NetworkController(config, paths)
+        paths.ensure_network_dirs()
 
-    # network_up(controller, config, paths)
-    clean_files(controller)
+        # network_up(controller, config, paths)
+        clean_files(controller)
+    except Exception as e:
+        co.errorln(f"{e}")
+        exit(1)
 
 if __name__ == "__main__":
     main()
