74e2c1dc6bee8e62b844b069c2a6bbbb84f04414
RianValcanaia <riancarlosvalcanaia99@gmail.com>
2026-01-16 13:18:52 -0300

    refact: rm infoln

--- src/config_loader.py
+++ src/config_loader.py
@@ -17,16 +17,14 @@ class ConfigLoader:
         self.full_config = {}
 
     def load(self):
-        # 1. Carrega topologia da rede
+        # Carerga a topologia da rede
         with open(self.network_config_path, 'r') as f:
             self.full_config['network_topology'] = yaml.safe_load(f)
-
-        # 2. Carrega versões
+        
+        # Carrega as versões do ambiente    
         if os.path.exists(self.versions_config_path):
             with open(self.versions_config_path, 'r') as f:
                 self.full_config['env_versions'] = yaml.safe_load(f)
         else:
             raise FileNotFoundError("Arquivo versions.yaml não encontrado!")
-
-        co.successln("Todas as configurações carregadas.")
         return self.full_config
